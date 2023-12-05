acec1c0ab70706b357aca2558784c09b0276139e
Jolvera2001 <jolvera6@stedwards.edu>
2023-12-05 00:19:48 -0600

    more testing needed for serving SPAs

--- goatappserver/src/main/kotlin/com/example/Application.kt
+++ goatappserver/src/main/kotlin/com/example/Application.kt
@@ -3,7 +3,10 @@ package com.example
 import com.example.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
+import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -11,6 +14,9 @@ fun main() {
 }
 
 fun Application.module() {
+    routing {
+
+    }
     configureSerialization()
     configureHTTP()
     configureSecurity()
--- goatappserver/src/main/kotlin/com/example/plugins/Routing.kt
+++ goatappserver/src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,25 +1,21 @@
 package com.example.plugins
 
 import io.ktor.http.*
+import io.ktor.http.ContentDisposition.Companion.File
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Application.configureRouting() {
     install(StatusPages) {
         exception<Throwable> { call, cause ->
             call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
         }
-    }
-    routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-        // Static plugin. Try to access `/static/index.html`
-        static("/static") {
-            resources("static")
+        exception<Throwable> { call, cause ->
+            call.respondText(text = "404: $cause", status = HttpStatusCode.NotFound)
         }
     }
 }
