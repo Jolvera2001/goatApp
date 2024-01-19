2e8e137644b624aba2286b382279b74549867946
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-01-19 12:56:57 -0600

    added in driver and setup

--- goatAppASP/goatAppASP/obj/Debug/net8.0/goatAppASP.AssemblyInfo.cs
+++ goatAppASP/goatAppASP/obj/Debug/net8.0/goatAppASP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("goatAppASP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8eebab846ef30c6eb5d265190f650af469fb1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("goatAppASP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("goatAppASP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
