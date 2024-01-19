c2209c714f690faa0140c706a5023c55598aea18
Jolvera2001 <143431900+Jolvera2001@users.noreply.github.com>
2024-01-19 15:36:27 -0600

    put in placeholder endpoints

--- goatAppASP/goatAppASP/Controllers/UserController.cs
+++ goatAppASP/goatAppASP/Controllers/UserController.cs
@@ -3,9 +3,23 @@
 namespace goatAppASP.Controllers
 {
     [ApiController]
-    [Route("UserCredentials/Login")]
+    [Route("[controller]/credentials")]
     public class UserController : ControllerBase
     {
+        [Route("login")]
+        [HttpPost]
+        public IActionResult Login([FromBody] Dictionary<string, string> formData)
+        {
+            return Ok("Login Successful");
+        }
+
+        [Route("register")]
+        [HttpPost]
+        public IActionResult register([FromBody] Dictionary<string, string> formData)
+        {
+            return Ok("Registration Successful");
+        }
+        
 
     }
 }
