89bbba2460c2c6c8c6fa5b3389ceef88eddfb97f
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-12 14:20:45 -0600

    Started PostServices

---
+++ goatAppASP/goatAppASP/Services/PostServices.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+
+namespace goatAppASP.Services
+{
+    public class PostServices
+    {
+        private readonly IMongoCollection<Post> _postCollection;
+
+        public PostServices(IMongoClient mongoClient)
+        {
+            var mongoDatabase = mongoClient.GetDatabase("");
+            _postCollection = mongoDatabase.GetCollection<Post>("");
+        }
+    }
+}
