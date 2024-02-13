5470171587ef9f59fe4bb9553e2ba69c2320a5df
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-13 12:20:00 -0600

    Added in data model for more CRUDs

---
+++ goatAppASP/goatAppASP/Models/Connections.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDbGenericRepository.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace goatAppASP.Models
+{
+    [CollectionName("UserConnections")]
+    public class Connections
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [Required]
+        [BsonElement("Following")]
+        public List<string> Following { get; set; }
+
+
+        [Required]
+        [BsonElement("Followers")]
+        public List<string> Followers { get; set; }
+    }
+}
