31b221b44187a6fccaaec4cb8a490528fa0b8d63
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-13 12:09:15 -0600

    Added in post CRUD services

--- goatAppASP/goatAppASP/Services/PostServices.cs
+++ goatAppASP/goatAppASP/Services/PostServices.cs
@@ -12,5 +12,23 @@ namespace goatAppASP.Services
             var mongoDatabase = mongoClient.GetDatabase("goat-places");
             _postCollection = mongoDatabase.GetCollection<Posts>("Posts");
         }
+
+        public async Task<List<Posts>> GetAsync() =>
+            await _postCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Posts?> GetAsync(string username) =>
+            await _postCollection.Find(x => x.UserName == username).FirstOrDefaultAsync();
+
+        public async Task<List<Posts>> GetAsyncList(string username) =>
+            await _postCollection.Find(x => x.UserName == username).ToListAsync();
+
+        public async Task CreateAsync(Posts newPost) =>
+            await _postCollection.InsertOneAsync(newPost);
+
+        public async Task UpdateAsync(string id, Posts updatedPost) =>
+            await _postCollection.ReplaceOneAsync(x => x.Id == id, updatedPost);
+
+        public async Task RemoveAsync(string id) =>
+            await _postCollection.DeleteOneAsync(x => x.Id == id);
     }
 }
--- goatAppASP/goatAppASP/Services/UserServices.cs
+++ goatAppASP/goatAppASP/Services/UserServices.cs
@@ -17,7 +17,7 @@ public class UserServices
     public async Task<List<User>> GetAsync() =>
             await _userCollection.Find(_ => true).ToListAsync();
 
-    public async Task<User?> GetAsync(String id) =>
+    public async Task<User?> GetAsync(string id) =>
         await _userCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
 
     public async Task<User?> GetAsyncName(string name) =>
@@ -26,9 +26,9 @@ public class UserServices
     public async Task CreateAsync(User newUser) =>
         await _userCollection.InsertOneAsync(newUser);
 
-    public async Task UpdateAsync(String id, User updatedUser) =>
+    public async Task UpdateAsync(string id, User updatedUser) =>
         await _userCollection.ReplaceOneAsync(x => x.Id == id, updatedUser);
 
-    public async Task RemoveAsync(String id) =>
+    public async Task RemoveAsync(string id) =>
         await _userCollection.DeleteOneAsync(x => x.Id == id);
 }
