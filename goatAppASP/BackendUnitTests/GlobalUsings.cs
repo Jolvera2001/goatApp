778009727a55c990a9ff9e1c7b8856cc0a6518c6
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-15 11:13:00 -0600

    Developing Unit tests

--- goatAppASP/BackendUnitTests/GlobalUsings.cs
+++ goatAppASP/BackendUnitTests/GlobalUsings.cs
@@ -1,1 +1,5 @@
 global using Xunit;
+global using FluentAssertions;
+global using System.Net.Http;
+global using System.Threading.Tasks;
+global using goatAppASP.Models;
--- goatAppASP/BackendUnitTests/LoginRegisterTests.cs
+++ goatAppASP/BackendUnitTests/LoginRegisterTests.cs
@@ -5,6 +5,33 @@ namespace BackendUnitTests
         [Fact]
         public void RegisterUsers()
         {
+            User user1 = new User
+            {
+                Id = "",
+                UserName = "JohnDevOps",
+                Password = "12345Typer",
+                Email = "John@Gmail.com",
+                FirstName = "John",
+                LastName = "DevOps"
+            };
+            User user2 = new User
+            {
+                Id = "",
+                UserName = "MiasmaMongo",
+                Password = "RunByJoji",
+                Email = "OfficialJoji@gmail.com",
+                FirstName = "Joji",
+                LastName = "Vlogs"
+            };
+            User user3 = new User
+            {
+                Id = "",
+                UserName = "",
+                Password = "12345Typer",
+                Email = "John@Gmail.com",
+                FirstName = "John",
+                LastName = "DevOps"
+            };
 
         }
 
