efbb82179a1c48bff8fc66ab45112cb157f4c2c3
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-15 12:23:01 -0600

    Completed Login UnitTest

--- goatAppASP/BackendUnitTests/GlobalUsings.cs
+++ goatAppASP/BackendUnitTests/GlobalUsings.cs
@@ -3,3 +3,5 @@ global using FluentAssertions;
 global using System.Net.Http;
 global using System.Threading.Tasks;
 global using goatAppASP.Models;
+global using MongoDB.Bson.IO;
+global using Newtonsoft.Json;
--- goatAppASP/BackendUnitTests/LoginRegisterTests.cs
+++ goatAppASP/BackendUnitTests/LoginRegisterTests.cs
@@ -1,3 +1,8 @@
+using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text;
+using JsonConvert = Newtonsoft.Json.JsonConvert;
+
 namespace BackendUnitTests
 {
     public class LoginRegisterTests
@@ -63,9 +68,44 @@ namespace BackendUnitTests
                 LastName = "J"
             };
 
-            // Setting up http client
+            // Setting up http client and Jsons
             var client = new HttpClient();
-            var url = "";
+            var url = "https://localhost:7023/user/credentials/login";
+
+            var json1 = JsonConvert.SerializeObject(user1);
+            var content1 = new StringContent(json1, Encoding.UTF8, "application/json");
+
+            var json2 = JsonConvert.SerializeObject(user2);
+            var content2 = new StringContent(json2, Encoding.UTF8, "application/json");
+
+            var json3 = JsonConvert.SerializeObject(user3);
+            var content3 = new StringContent(json3, Encoding.UTF8, "application/json");
+
+            var badJson1 = JsonConvert.SerializeObject(badUser1);
+            var badContent1 = new StringContent(badJson1, Encoding.UTF8, "application/json");
+
+            var badJson2 = JsonConvert.SerializeObject(badUser2);
+            var badContent2 = new StringContent(badJson2, Encoding.UTF8, "application/json");
+
+            var badJson3 = JsonConvert.SerializeObject(badUser3);
+            var badContent3 = new StringContent(badJson3, Encoding.UTF8, "application/json");
+
+            // testing
+            var response1 = await client.PostAsync(url, content1);
+            var response2 = await client.PostAsync(url, content2);
+            var response3 = await client.PostAsync(url, content3);
+
+            var response4 = await client.PostAsync(url, badContent1);
+            var response5 = await client.PostAsync(url, badContent2);
+            var response6 = await client.PostAsync(url, badContent3);
+
+            response1.EnsureSuccessStatusCode();
+            response2.EnsureSuccessStatusCode();
+            response3.EnsureSuccessStatusCode();
+
+            Assert.Equal(HttpStatusCode.BadRequest, response4.StatusCode);
+            Assert.Equal(HttpStatusCode.Conflict, response5.StatusCode);
+            Assert.Equal(HttpStatusCode.BadRequest, response6.StatusCode);
 
         }
 
