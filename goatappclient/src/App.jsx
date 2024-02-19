3d5054dde72c88d92224dbd0ddc66dd1512305b1
Jolvera2001 <jolvera6@stedwards.edu>
2024-02-18 18:05:56 -0600

    fixed up the routing

--- goatappclient/src/App.jsx
+++ goatappclient/src/App.jsx
@@ -7,20 +7,24 @@ import Account from './pages/Account';
 import CreatePost from './pages/CreatePost'
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<LandingPageRedone />} />
-        <Route path='/homepage' element={<Homepage />} />
-        <Route path='/mapPage' element={<MapPage />} />
-        <Route path='/account' element={<Account />} />
-        <Route path='/create-post' element={<CreatePost />} />
+        <Route path="/" element={<LandingPageRedone />} />
+      </Routes>
+      <Sidebar />
+      <Routes>
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/mapPage" element={<MapPage />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/create-post" element={<CreatePost />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App
--- goatappclient/src/main.jsx
+++ goatappclient/src/main.jsx
@@ -4,54 +4,9 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom';
 
 import App from './App.jsx';
-import Sidebar from './components/Sidebar.jsx';
-import Homepage from './pages/Homepage.jsx';
-import Account from './pages/Account.jsx';
-import MapPage from './pages/MapPage.jsx';
-import LandingPageRedone from './pages/LandingPageRedone';
-import CreatePost from './pages/CreatePost'
-
-
-
-const AppLayout = () => (
-  <>
-    {/* Conditionally render Sidebar based on route */}
-    {window.location.pathname !== '/' && <Sidebar />}
-    <Outlet />
-  </>
-);
-
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    children: [
-      {
-        path: '/',
-        element: <LandingPageRedone />,
-      },
-      {
-        path: 'homepage',
-        element: <Homepage />,
-      },
-      {
-        path: 'account',
-        element: <Account />,
-      },
-      {
-        path: 'map',
-        element: <MapPage />,
-      },
-      {
-        path: 'create-post',
-        element: <CreatePost />,
-      },
-    ],
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <App />
 );
